3f8b397d7b5bead78aac877547fee91fc20feb8e
Rakhapy <rakhapy@gmail.com>
2025-03-01 23:30:41 +0700

    using next img

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,14 @@
 // "use client"
 import Link from "next/link";
 import Button from "./components/button/button";
+import Image from "next/image";
 // import { redirect } from 'next/navigation'
 
 
 export default function Home() {
   return (
     <div className="flex justify-center gap-1 items-center flex-col min-h-screen sm:p-10">
-      <img src="https://www.svgrepo.com/show/483652/hacker.svg" width={100} alt="" />
+      <Image src="https://www.svgrepo.com/show/483652/hacker.svg" width={100} height={100} alt="" />
       <h1 className=" text-2xl font-bold" > This Server is Fucking Down</h1>
       <h2>Go away from here kid</h2>
       <Link href={"pages/main"} >Go away</Link>
